872b6b9606ac7708155224e1a8e2549c4d1161f6
oscarbaltasar <oscarbaltasar@hotmail.com>
2022-05-06 11:37:48 +0200

    EnemySpawner añadido

--- enemySpawner.cpp
+++ enemySpawner.cpp
@@ -7,7 +7,7 @@ void EnemySpawner::step()
 	i++;
 	if (i % randomSpawn == 0) {
 		randomSpawn = rand() % 70 + 50;
-		Enemy* enemy = new Enemy("bullet.trg");
+		Enemy* enemy = new Enemy("triangle.trg");
 		enemy->scale = this->scale;
 		enemy->position.y = this->position.y;
 		enemy->rotation.z = this->rotation.z;
